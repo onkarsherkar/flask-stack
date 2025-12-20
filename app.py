1f04d53019047f410a73eaa21f384fd69d9919a2
onkarsherkar <onkar.sherkar@gmail.com>
2025-12-20 15:22:39 +0530

    routing

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "<h1>Hello Flask</h1>"
+
+
+@app.route("/home")
+def home():
+    return "<h2>Home</h2>"
+
+
+@app.route("/json")
+def json():
+    return {"mykey": "json value!", "mylist": [1, 2, 3, 4]}
