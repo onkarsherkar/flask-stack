6c252e6cf634e77ee146d8711edb82e139926785
onkarsherkar <onkar.sherkar@gmail.com>
2025-12-20 15:48:45 +0530

    route variable

--- app.py
+++ app.py
@@ -16,3 +16,9 @@ def home():
 @app.route("/json")
 def json():
     return {"mykey": "json value!", "mylist": [1, 2, 3, 4]}
+
+
+@app.route("/dynamic", defaults={"user_input": "default"})
+@app.route("/dynamic/<user_input>")
+def dynamic(user_input):
+    return f"<h2>The usered eneter text:{user_input} </h2>"
